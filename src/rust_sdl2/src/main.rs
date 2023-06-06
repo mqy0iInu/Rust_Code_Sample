e5e21ac7a8e692dfdf7573038408cf55bb54b0ae
FYuto <kr84Tsroot@gmail.com>
2023-06-07 00:46:53 +0900

    fix Bug

--- src/rust_sdl2/src/main.rs
+++ src/rust_sdl2/src/main.rs
@@ -13,33 +13,68 @@ use sdl2::keyboard::Scancode;
 const KEY_MOVE_VAL      :u32  = 10;
 const WINDOW_WIDTH      :u32 = 800;
 const WINDOW_HIGHT      :u32 = 600;
-const CHAR_MOVE_WIDTH   :u32 = 350;
-const CHAR_MOVE_HIGHT   :u32 = 250;
+const CHAR_WIDTH        :u32 = 350;
+const CHAR_HIGHT        :u32 = 250;
 
-pub fn main() {
+const SAFE_ADD :u8 = 0;
+const SAFE_SUB :u8 = 1;
+const SAFE_X :u8 = 0;
+const SAFE_Y :u8 = 1;
+
+pub fn safe_add_sub(xy :u32, val :u32, xy_type :u8, mode :u8,) -> u32
+{
+    if mode != SAFE_SUB {
+        if xy_type != SAFE_Y
+        {
+            if xy >= (WINDOW_WIDTH - 100) {
+                WINDOW_WIDTH - 100
+            }else {
+                xy + val
+            }
+        }else {
+            if xy >= (WINDOW_HIGHT - 100) {
+                WINDOW_HIGHT - 100
+            }else {
+                xy + val
+            }
+        }
+    }else {
+        if xy > val {
+            xy - val
+        }else {
+            0
+        }
+    }
+}
+
+pub fn main()
+{
     let sdl_context = sdl2::init().unwrap();
     let video_subsystem = sdl_context.video().unwrap();
     let window = video_subsystem
-        .window("Rust-SDL2", WINDOW_WIDTH, WINDOW_HIGHT)
+        .window("Rust_SDL2", WINDOW_WIDTH, WINDOW_HIGHT)
         .position_centered().build().unwrap();
     let mut canvas = window.into_canvas().build().unwrap();
     let mut event_pump = sdl_context.event_pump().unwrap();
-    let (mut x, mut y) = (CHAR_MOVE_WIDTH, CHAR_MOVE_HIGHT);
+    let (mut x, mut y) = (CHAR_WIDTH, CHAR_HIGHT);
 
-    'running: loop {
+    'lab_loop: loop
+    {
         let state = event_pump.keyboard_state();
         if state.is_scancode_pressed(Scancode::Up) {
-            y -= KEY_MOVE_VAL;
+            y = safe_add_sub(y, KEY_MOVE_VAL, SAFE_Y, SAFE_SUB);
         }
         if state.is_scancode_pressed(Scancode::Down) {
-            y += KEY_MOVE_VAL;
+            y = safe_add_sub(y, KEY_MOVE_VAL, SAFE_Y, SAFE_ADD);
         }
         if state.is_scancode_pressed(Scancode::Left) {
-            x -= KEY_MOVE_VAL;
+            x = safe_add_sub(x, KEY_MOVE_VAL, SAFE_X, SAFE_SUB);
         }
         if state.is_scancode_pressed(Scancode::Right) {
-            x += KEY_MOVE_VAL;
+            x = safe_add_sub(x, KEY_MOVE_VAL, SAFE_X, SAFE_ADD);
         }
+
+        println!("X: {}, y: {}", x, y);
 
         for event in event_pump.poll_iter() {
             match event {
@@ -47,7 +82,7 @@ pub fn main() {
                 | Event::KeyDown {
                     keycode: Some(Keycode::Escape),
                     ..
-                } => break 'running,
+                } => {break 'lab_loop},
                 _ => {}
             }
         }
@@ -59,6 +94,7 @@ pub fn main() {
         canvas.fill_rect(Rect::new(x.try_into().unwrap(), y.try_into().unwrap(), 100, 100)).unwrap();
 
         canvas.present();
-        std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
+
+        std::thread::sleep(Duration::new(0, 10_0000_0000u32 / 60));
     }
 }
